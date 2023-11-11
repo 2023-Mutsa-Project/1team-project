1ea7909fb11266b198301a4a58cbae349dd728a3
hyer2 <snake0113@naver.com>
2023-11-12 05:01:03 +0900

    navBar-ing

--- slimtime/src/Navbar.js
+++ slimtime/src/Navbar.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import {Button} from './Button';
+import logoImage from "./img/logo.png";
 import './Navbar.css';
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -35,35 +37,45 @@ function Navbar() {
             <div className = 'navbar-container'>
                 {/* 모바일버전에서 클릭하면 메뉴 보이도록 설정하는 것도 한다. (close Mobile Menu)는 다시 버튼 누르면 없어지고 생기고 하도록 한다.  */}
                 <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                    TRVL 
-                    <i className='fab fa-typo3' />
+                    <img src={logoImage} alt='Logo' />
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
-                    <i className = {click ? 'fas fa-times' : 'fas fa-bars' } />
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick = {closeMobileMenu}>
-                            Home
+                        <Link to='/search' className='nav-links' onClick = {closeMobileMenu}>
+                            Diet 보조제 서치
                         </Link>
                     </li>
                     <li className='nav-item'>
                         <Link to='/services' className='nav-links' onClick = {closeMobileMenu}>
-                            Services
+                            Diet 보조제 리뷰
                         </Link>
                     </li>
                     <li className='nav-item'>
                         <Link to='/products' className='nav-links' onClick = {closeMobileMenu}>
-                            Products
+                            Diet 챌린지
                         </Link>
                     </li>
                     <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick = {closeMobileMenu}>
+                            Diet  토크
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/login' className='nav-links-mobile' onClick = {closeMobileMenu}>
+                            로그인
+                        </Link>
+                    </li>
+                    <li>
                         <Link to='/sign-up' className='nav-links-mobile' onClick = {closeMobileMenu}>
-                            Sign Up
+                            회원가입
                         </Link>
                     </li>
                 </ul>
-                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                {button && <Button buttonStyle='btn--inline'>로그인</Button>}
+                {button && <Button buttonStyle='btn--inline'>회원가입</Button>}
             </div>
         </nav>
         </>
