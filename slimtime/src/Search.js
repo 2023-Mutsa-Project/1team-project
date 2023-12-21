f9cd42690928e48f9b00df7e4ffaeecd9212d8b2
hyer2 <snake0113@naver.com>
2023-12-21 16:21:14 +0900

    서치페이지 작업 중

--- slimtime/src/Search.js
+++ slimtime/src/Search.js
@@ -1,9 +1,9 @@
 "use client";
 import React from 'react';
 import Navbar from'./comp/Navbar';
-import './Main.css';
-import subtitleImg from './img/mainsubtitle.png'
-
+import ReviewCard from'./comp/Reviewcard';
+import './Search.css';
+import Footer from'./comp/footer';
 
 function Search() {
     return (
@@ -39,12 +39,46 @@ function Main() {
             <Navbar />
             <Search />
         </div>
-        <div className="pick-container">
-            <img src={subtitleImg} alt="Main Subtitle" className='subtitleImg'/>
-            <div className="subtxt">
-                <p>슬림타임이 추천하는 다이어트 보조제 Top 10을 만나보세요. </p>
-                <p>슬림타이머들이 남긴 평점 및 리뷰 기반으로 추천해드립니다.</p>
+        <div className="data-container">
+            <div className="filter-container">
+              <div className="filter-btn">
+                <p>리뷰 많은 순</p>
+              </div>
+              <div className="filter-btn">
+                <p>높은 평점 순</p>
+              </div>
+              <div className="filter-btn">
+                <p>낮은 가격 순</p>
+              </div>
             </div>
+            <div className="price-container">
+              <div className="price-btn">
+              <div className="price-btn1">
+                <p>2만 5천원 이하</p>
+              </div>
+              <div className="section">
+                <p>ㅣ</p>
+              </div>
+              <div className="price-btn2">
+                <p>2만 5천원~5만원</p>
+              </div>
+              <div className="section">
+                <p>ㅣ</p>
+              </div>
+              <div className="price-btn3">
+                <p>5만원 이상</p>
+              </div>
+              </div>
+            </div>
+            <div className="card-container" style={{ display: 'flex', gap: '45px' }}>
+                
+                <ReviewCard/>
+                <ReviewCard/>
+                <ReviewCard/>
+            </div>
+        </div>
+        <div className='footer-container'>
+            <Footer/>
         </div>
     </div>
   );
