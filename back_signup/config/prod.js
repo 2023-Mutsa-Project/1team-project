38340fbe5075cecca960b72efe3c4202b4c4002d
Gaeun <bke2637@gmail.com>
2023-12-28 14:19:44 +0900

    mongoURI

--- back_signup/config/prod.js
+++ back_signup/config/prod.js
@@ -1,4 +1,12 @@
 module.exports = {
-  // HEROKU Config Vars = MONGO_URI 이름 똑같이 설정 
-  mongoURI: process.env.MONGO_URI
-}
+  mongoURI:
+    "mongodb+srv://Kangjijhye:KvAlworelzt0CSdO@1team.kr1i2d4.mongodb.net/",
+  connectMongoDB: async () => {
+    try {
+      await mongoose.connect(module.exports.mongoURI);
+      console.log("MongoDB connected");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
